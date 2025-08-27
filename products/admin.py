2de74d82638e18e9126d9a35fbea99bed3c8512a
Krishna Varma Vuyyuri <119467671+KrishnaVarmaVuyyuri@users.noreply.github.com>
2025-08-27 17:11:46 +0530

    Update admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import Product
+from django.utils.html import format_html
 
-@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "price", "stock", "created_at")
-    search_fields = ("name", "description")
-    list_filter = ("created_at",)
-    ordering = ("-created_at",)
+    list_display = ("name", "price", "created_at", "preview")
+
+    def preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" width="60" height="60" />', obj.image.url)
+        return "No Image"
+
+admin.site.register(Product, ProductAdmin)
