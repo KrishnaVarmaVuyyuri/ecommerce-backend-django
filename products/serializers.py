51073309bba5600e44e83e0d9dfa398bb7c6bcb0
Krishna Varma Vuyyuri <119467671+KrishnaVarmaVuyyuri@users.noreply.github.com>
2025-08-27 18:30:05 +0530

    Update serializers.py

--- products/serializers.py
+++ products/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from .models import Product
 
 class ProductSerializer(serializers.ModelSerializer):
+    image = serializers.ImageField(use_url=True)  # ensures URL is returned
+
     class Meta:
         model = Product
-        fields = ["id", "name", "description", "price", "image", "stock", "created_at"]
+        fields = "__all__"
+
